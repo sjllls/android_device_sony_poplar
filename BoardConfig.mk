a44280b9a90429f8a8e85a1b489087fbc7c94234
Sjll <2012ji123@gmail.com>
2019-01-14 23:50:30 +0800

    Clean up

---
+++ BoardConfig.mk
@@ -0,0 +1,53 @@
+# Copyright 2014 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+include device/sony/yoshino/PlatformConfig.mk
+
+TARGET_BOOTLOADER_BOARD_NAME := unknown
+ifneq (,$(filter %g8341,$(TARGET_PRODUCT)))
+TARGET_BOOTLOADER_BOARD_NAME := G8341
+else ifneq (,$(filter %g8342,$(TARGET_PRODUCT)))
+TARGET_BOOTLOADER_BOARD_NAME := G8342
+else
+TARGET_BOOTLOADER_BOARD_NAME := G8342
+$(warning Unrecognized value for TARGET_PRODUCT: "$(TARGET_PRODUCT)", using default value: "$(TARGET_BOOTLOADER_BOARD_NAME)")
+endif
+
+# Platform
+PRODUCT_PLATFORM := yoshino
+
+# NFC
+NXP_CHIP_TYPE := PN553
+NXP_CHIP_FW_TYPE := PN553
+
+BOARD_KERNEL_CMDLINE += androidboot.hardware=poplar
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
+
+# Partition information
+BOARD_FLASH_BLOCK_SIZE := 131072 # (BOARD_KERNEL_PAGESIZE * 64)
+BOARD_CACHEIMAGE_PARTITION_SIZE := 398458880
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 5242880000
+# Reserve space for data encryption (55743832064-32768)
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 55743799296
+
+# Build a separate vendor.img
+TARGET_COPY_OUT_VENDOR := vendor
+BOARD_VENDORIMAGE_PARTITION_SIZE := 1610612736
+BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_ROOT_EXTRA_SYMLINKS := /vendor/lib/dsp:/dsp
+BOARD_PROPERTY_OVERRIDES_SPLIT_ENABLED := true
+
+TARGET_USES_CASH_EXTENSION := true
+
+#TARGET_TAP_TO_WAKE_NODE := "/sys/devices/virtual/input/clearpad/wakeup_gesture"
---
+++ omni_poplar.mk
@@ -0,0 +1,34 @@
+# Copyright 2014 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Bootanimation
+TARGET_BOOTANIMATION_SIZE := 1080p
+
+# Inherit device parts
+$(call inherit-product, device/sony/poplar/aosp_g8342.mk)
+
+# Override Product Name
+PRODUCT_NAME := omni_poplar
+PRODUCT_MODEL := Xperia XZ1
+
+# Assert
+TARGET_OTA_ASSERT_DEVICE := none
+
+# HAL3
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.camera.HAL3.enabled=1
+
+# Inherit rom parts
+$(call inherit-product, device/sony/common/omni.mk)
+$(call inherit-product, vendor/omni/config/gsm.mk)
